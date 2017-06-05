0744d403f5b4c7ffae8ae709aa8661fefff0537c
logzero <core13@gmx.net>
2017-06-05 12:37:25 +0200

    Decouple sample number from canvas size. Reduce samples to 256.

--- tirepn.py
+++ tirepn.py
@@ -132,7 +132,7 @@ def ComputePatch(coeff, fz):
     w = coeff['width']
     ar = coeff['ar']
     pt = coeff['pt']
-    camber = coeff['camber'] * pi / 180
+    camber = -coeff['camber'] * pi / 180.0
     ccb = coeff['ccb']
     #cfz = coeff['cfz']
     #fz0 = coeff['fz0']
@@ -733,9 +733,10 @@ def addSlider(parent, txt, val, vmin, vmax, vinfo, call):
 class App:
     def __init__(self, root):
         self.need_redraw = False
-        self.samples = 512
-        self.slip_angle_scale = (60.0 / 180.0 * pi) / self.samples
-        self.slip_scale = 2.0 / self.samples
+        self.canvas_size = 512
+        self.samples = 256
+        self.slip_max = 1.0
+        self.slip_angle_max = 30.0
         self.coeff = coeff_default.copy()
         self.coeff0 = {}
         self.file_opt = {
@@ -765,7 +766,7 @@ class App:
         cframe = Frame(frame.interior, width=512)
         cframe.pack(side=TOP, anchor=NW)
 
-        self.canvas = Canvas(cframe, width=512, height=512)
+        self.canvas = Canvas(cframe, width=self.canvas_size, height=self.canvas_size)
         self.canvas.pack(fill=BOTH, expand=YES)
 
         # load/save buttons
@@ -822,70 +823,74 @@ class App:
             f.write('{0}={1:f}\n'.format(n, self.coeff[n]))
 
     def sampleData0(self, coeff):
+        slip_angle_max = self.slip_angle_max / 180.0 * pi
+        scale = 0.5 * self.canvas_size
+        scalea = scale / slip_angle_max
+        scales = scale / self.slip_max
+        scalef = 0.5 * scale
+        slip_angle_scale = 2.0 * slip_angle_max / self.samples
+        slip_scale = 2.0 * self.slip_max / self.samples
+        camber = -self.coeff['camber'] / 180.0 * pi
+        sa = self.coeff['slip angle'] / 180.0 * pi
         fz = self.coeff['fz']
-        camber = self.coeff['camber'] / 180.0 * pi
-        slip_angle = self.coeff['slip angle'] / 180.0 * pi
-        samples = self.samples
-        samples2 = self.samples / 2
-        # force, torque curves
-        afx, afy, amz = [], [], []
-        for i in range(0, samples, 1):
-            s = self.slip_scale * (i - samples2)
-            a = self.slip_angle_scale * (i - samples2)
-            fx, fy, mz = Pacejka(coeff, s, a, camber, fz)
-            afx.append((i, samples2 * (1 - 0.5 * fx / fz)))
-            afy.append((i, samples2 * (1 + 0.5 * fy / fz)))
-            amz.append((i, samples2 * (1 +  10 * mz / fz)))
-        # combined force curve
         dfz = PacejkadFz(coeff, fz)
-        fy, dy, bcy, shf = PacejkaFy(coeff, slip_angle, camber, fz, dfz)
-        atcp = []
-        for i in range(0, samples, 1):
-            s = self.slip_scale * (i - samples2)
-            fx = 2 * fz * (1 - afx[i][1] / samples2)
-            fcx, fcy = PacejkaCombine(coeff, s, slip_angle, camber, fx, fy, dy, dfz)
-            atcp.append((samples2 * (1 - 0.5 * fcx / fz), samples2 * (1 - 0.5 * fcy / fz)))
-        fy, dy, bcy, shf = PacejkaFy(coeff, -slip_angle, camber, fz, dfz)
-        atcn = []
-        for i in range(0, samples, 1):
-            s = self.slip_scale * (i - samples2)
-            fx = 2 * fz * (1 - afx[samples - 1 - i][1] / samples2)
-            fcx, fcy = PacejkaCombine(coeff, s, -slip_angle, camber, fx, fy, dy, dfz)
-            atcn.append((samples2 * (1 - 0.5 * fcx / fz), samples2 * (1 - 0.5 * fcy / fz)))
-        return afx, afy, amz, atcp, atcn
+        fyp, dyp, bcyp, shfp = PacejkaFy(coeff, sa, camber, fz, dfz)
+        fyn, dyn, bcyn, shfn = PacejkaFy(coeff, -sa, camber, fz, dfz)
+        afx, afy, amz, acp, acn = [], [], [], [], []
+        for i in range(-self.samples // 2, self.samples // 2, 1):
+            s = i * slip_scale
+            a = i * slip_angle_scale
+            fx = PacejkaFx(coeff, s, fz, dfz)
+            fy, dy, bcy, shf = PacejkaFy(coeff, a, camber, fz, dfz)
+            mz = PacejkaMz(coeff, a, camber, fz, dfz, fy, bcy, shf)
+            gx = PacejkaGx(coeff, s, sa)
+            gy = PacejkaGy(coeff, s, sa)
+            svyp = PacejkaSvy(coeff, s, sa, camber, dyp, dfz)
+            svyn = PacejkaSvy(coeff, s, sa, camber, dyn, dfz)
+            mux = gx * fx / fz
+            muyp = (gy * fyp + svyp) / fz
+            muyn = (gy * fyn + svyn) / fz
+            afx.append((scale + s * scales, (2 - fx / fz) * scalef))
+            afy.append((scale + a * scalea, (2 + fy / fz) * scalef))
+            amz.append((scale + a * scalea, (2 + 10 * mz / fz) * scalef))
+            acp.append(((2 - mux) * scalef, (2 - muyp) * scalef))
+            acn.append(((2 - mux) * scalef, (2 - muyn) * scalef))
+        return afx, afy, amz, acp, acn
 
     def sampleData(self, coeff):
+        slip_angle_max = self.slip_angle_max / 180.0 * pi
+        scale = 0.5 * self.canvas_size
+        scalea = scale / slip_angle_max
+        scales = scale / self.slip_max
+        scalef = 0.5 * scale
+        slip_angle_scale = 2.0 * slip_angle_max / self.samples
+        slip_scale = 2.0 * self.slip_max / self.samples
+        sa = self.coeff['slip angle'] / 180.0 * pi
         fz = self.coeff['fz']
-        slip_angle = self.coeff['slip angle'] / 180.0 * pi
-        samples = self.samples
-        samples2 = self.samples / 2
         # force, torque curves
         afx, afy, amz = [], [], []
         patch = ComputePatch(coeff, fz)
-        for i in range(1, samples - 1, 1):
-            s = self.slip_scale * (i - samples2)
-            a = self.slip_angle_scale * (i - samples2)
+        for i in range(-self.samples // 2, self.samples // 2, 1):
+            s = i * slip_scale
+            a = i * slip_angle_scale
             fx = ComputeFx(coeff, patch, s)
             fy, mz = ComputeFy(coeff, patch, a)
-            afx.append((i, samples2 * (1 - 0.5 * fx / fz)))
-            afy.append((i, samples2 * (1 + 0.5 * fy / fz)))
-            amz.append((i, samples2 * (1 +  10 * mz / fz)))
+            afx.append((scale + s * scales, (2 - fx / fz) * scalef))
+            afy.append((scale + a * scalea, (2 + fy / fz) * scalef))
+            amz.append((scale + a * scalea, (2 + 10 * mz / fz) * scalef))
         # combined force curve
-        atcp = []
-        for i in range(1, samples - 1, 1):
-            s = self.slip_scale * (i - samples2)
-            fx, fy, mz = friction(coeff, patch, s, slip_angle)
-            atcp.append((samples2 * (1 - 0.5 * fx / fz), samples2 * (1 - 0.5 * fy / fz)))
-        atcn = []
-        for i in range(1, samples - 1, 1):
-            s = self.slip_scale * (i - samples2)
-            fx, fy, mz = friction(coeff, patch, s, -slip_angle)
-            atcn.append((samples2 * (1 - 0.5 * fx / fz), samples2 * (1 - 0.5 * fy / fz)))
+        atcp, atcn = [], []
+        for i in range(-self.samples // 2, self.samples // 2, 1):
+            s = i * slip_scale
+            fxp, fyp, _ = friction(coeff, patch, s, sa)
+            fxn, fyn, _ = friction(coeff, patch, s, -sa)
+            atcp.append(((2 - fxp / fz) * scalef, (2 - fyp / fz) * scalef))
+            atcn.append(((2 - fxn / fz) * scalef, (2 - fyn / fz) * scalef))
         return afx, afy, amz, atcp, atcn
         
     def updateCanvas(self):
         # clear canvas
-        s = self.samples
+        s = self.canvas_size
         self.canvas.delete(ALL)
         self.canvas.create_line(s/4, 2, s/4, s, width=1, fill="grey")
         self.canvas.create_line(s/2, 2, s/2, s, width=1, fill="black")
